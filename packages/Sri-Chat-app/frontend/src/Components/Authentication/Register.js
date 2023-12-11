898c1b36d5f59bc06d3ef5bdf02edae4113bd4fb
Srilu1238 <srilu1238@gmail.com>
2023-12-11 23:47:19 +0000

    Login, Registration pages frontend updated.

---
+++ packages/Sri-Chat-app/frontend/src/Components/Authentication/Register.js
@@ -0,0 +1,81 @@
+import { Button, FormControl, FormHelperText, FormLabel, Input, InputGroup, InputRightElement, VStack } from '@chakra-ui/react';
+import React, { useState } from 'react';
+
+const Register = () => {
+    const [show, setShow] = useState(false);
+    const [name, setName] = useState();
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const [confirmpassword, setConfirmPassword] = useState();
+    const [pic, setPic] = useState();
+    const handleClick = () => setShow(!show);
+    const postDetails = (pics) => { };
+    const submitHandler = () => { };
+    return <VStack spacing='5px'  >
+        <FormControl id='full-name' isRequired>
+            <FormLabel>FullName</FormLabel>
+            <Input placeholder='FullName'
+                onChange={(e) => setName(e.target.value)} />
+        </FormControl>
+
+        <FormControl id='email' isRequired>
+            <FormLabel>Email</FormLabel>
+
+            <Input placeholder='Email'
+                onChange={(e) => setEmail(e.target.value)} />
+        </FormControl>
+
+        <FormControl id='password' isRequired>
+            <FormLabel>Password</FormLabel>
+
+            <InputGroup>
+                <Input
+                    type={show ? "text" : "password"}
+                    placeholder='Password'
+                    onChange={(e) => setPassword(e.target.value)} />
+                <InputRightElement width="4.5rem">
+                    <Button h="1.75rem" size="sm" onClick={handleClick}>
+                        {show ? "Hide" : "Show"}
+                    </Button>
+                </InputRightElement>
+            </InputGroup>
+        </FormControl>
+
+        <FormControl id='password' isRequired>
+            <FormLabel>Confirm Password</FormLabel>
+
+            <InputGroup size="md">
+                <Input
+                    type={show ? "text" : "password"}
+                    placeholder='Confirm Password'
+                    onChange={(e) => setConfirmPassword(e.target.value)} />
+                <InputRightElement width="4.5rem">
+                    <Button h="1.75rem" size="sm" onClick={handleClick}>
+                        {show ? "Hide" : "Show"}
+                    </Button>
+                </InputRightElement>
+            </InputGroup>
+        </FormControl>
+
+        <FormControl id="pic">
+            <FormLabel>Upload you Picture</FormLabel>
+            <Input
+                type='file'
+                p={1.5}
+                accept='image/*'
+                onChange={(e) => postDetails(e.target.files[0])}
+            />
+        </FormControl>
+
+        <Button
+            colorScheme='blue'
+            width='50%'
+            style={{ marginTop: 15 }}
+            onClick={submitHandler}
+
+        >Register</Button>
+
+    </VStack>
+}
+
+export default Register;
--- packages/Sri-Chat-app/frontend/src/Pages/Home.js
+++ packages/Sri-Chat-app/frontend/src/Pages/Home.js
@@ -1,10 +1,40 @@
 import React from 'react';
-
+import { Container, Box, Text, Tab, TabList, TabPanel, TabPanels, Tabs, Center } from "@chakra-ui/react";
+import Register from '../Components/Authentication/Register';
+import Login from '../Components/Authentication/Login';
 const Home = () => {
     return (
-        <div>
-            Home
-        </div>
+        <Container maxW='xl' centerContent >
+            <Box d='flex'
+                justifyContent='center'
+                fontFamily="Roboto"
+                p={3}
+                bg={"white"}
+                w="100%"
+                m="40px 0 15px 0"
+                borderRadius="1g"
+                borderWidth="1px"
+            >
+                <Text align="center" fontSize="4xl" fontFamily="Roboto" color="#70ccd0" fontStyle="italic
+                " marginBottom="10px">Sri-Chat-App</Text>
+
+                <Tabs isFitted variant='enclosed' >
+                    <TabList mb='1em' >
+                        <Tab >Login</Tab>
+                        <Tab backgroundColor='#70ccd0' color='white'>Register</Tab>
+                    </TabList>
+                    <TabPanels>
+                        <TabPanel >
+                            <Login />
+                        </TabPanel>
+                        <TabPanel >
+                            <Register />
+                        </TabPanel>
+                    </TabPanels>
+                </Tabs>
+
+            </Box>
+        </Container>
     );
 }
 
