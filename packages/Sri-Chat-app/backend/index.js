415de5d60efce30cd391931a1b0f88f9902d88e7
Srilu1238 <srilu1238@gmail.com>
2024-01-20 21:32:46 +0000

    updated frontend/index.js

--- packages/Sri-Chat-app/backend/index.js
+++ packages/Sri-Chat-app/backend/index.js
@@ -48,7 +48,7 @@ app.use('/api/message', messageRoutes);
 const path = require("path");
 const __dirname1 = path.resolve();
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "build")));
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
