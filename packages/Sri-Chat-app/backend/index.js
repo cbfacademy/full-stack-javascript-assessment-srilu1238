675b26b5e6e2ebe9045ca397c69c51d332da2ad8
srilekha <srilu1238@yahoo.co.in>
2024-01-21 13:25:23 +0000

    Update index.js

--- packages/Sri-Chat-app/backend/index.js
+++ packages/Sri-Chat-app/backend/index.js
@@ -53,10 +53,10 @@ app.use('api/message', messageRoutes);
 const __dirname1 = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/package/Sri-Chat-app/frontend/build")));
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "package","Sri-Chat-app","frontend", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname1,"frontend", "build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
