d8e02b3ca4b519d82282392cf8ccaab4da97509c
Srilu1238 <srilu1238@gmail.com>
2024-01-24 11:45:17 +0000

    updated index.js in backend

--- packages/Sri-Chat-app/backend/index.js
+++ packages/Sri-Chat-app/backend/index.js
@@ -73,10 +73,10 @@ io.on("connection", (socket) => {
   const __dirname1 = path.resolve();
 
   if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname1, "/frontend", "/build")));
+    app.use(express.static(path.join(__dirname1, "https://sri-chat-app-frontend-static.onrender.com")));
 
     app.get("*", (req, res) =>
-      res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+      res.sendFile(path.resolve(__dirname1, `https://sri-chat-app-frontend-static.onrender.com/build/index.html`))
     );
   } else {
     app.get("/api", (req, res) => {
